ef30d40b54a4075055457787fea39dd0748eeb8b
Howard <howardkong826@gmail.com>
2025-11-29 01:00:43 -0500

    feat: upload favicon

--- assets/inc/nav.inc.php
+++ assets/inc/nav.inc.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $pageInfo['title']; ?></title>
+  <link rel="icon" href="<?php echo $path; ?>assets/images/favicon.png">
   <link rel="stylesheet" href="<?php echo $path; ?>assets/css/theme.css">
   <link rel="stylesheet" href="<?php echo $path; ?>assets/css/grid.css">
   <link rel="stylesheet" href="<?php echo $path; ?>assets/css/animation.css">
